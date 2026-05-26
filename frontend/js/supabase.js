ba9f43b70d3587ec4f0261a951504067cf07a753
Sam Naughty <sam@naughtythings.com>
2026-05-26 18:16:02 +0800

    Fix Supabase Anon Key

--- frontend/js/supabase.js
+++ frontend/js/supabase.js
@@ -3,7 +3,7 @@
 // ============================================
 
 const SUPABASE_URL = 'https://aymwuafwsbtjmjfmkzbf.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbG…RGk8';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF5bXd1YWZ3c2J0am1qZm1remJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk3ODA2MzIsImV4cCI6MjA5NTM1NjYzMn0.CyiC1b_B4kjaQgE0t5LkDiAhUvG8JQ9hns2Uk6CRGk8';
 
 let supabaseClient = null;
 
